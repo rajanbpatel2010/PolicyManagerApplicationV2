8bdd8798e6355e1a31664d0e3373647d1c58c952
Rajan Patel <rajanbpatel2017@gmail.com>
2026-06-20 19:50:31 +0530

    Fix missing port parsing logic in database connection string

--- backend/PolicyManager.API/Program.cs
+++ backend/PolicyManager.API/Program.cs
@@ -94,7 +94,8 @@ try
     {
         var uri = new Uri(databaseUrl);
         var userInfo = uri.UserInfo.Split(':');
-        connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.LocalPath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true;";
+        var port = uri.Port > 0 ? uri.Port : 5432;
+        connectionString = $"Host={uri.Host};Port={port};Database={uri.LocalPath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true;";
     }
 
     builder.Services.AddDbContext<AppDbContext>(options =>
